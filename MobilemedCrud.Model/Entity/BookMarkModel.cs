a3f490dad6abd6ae7323849e5a0dfa2d067163ca
Eduardo Matsutani <duty274.edu@gmail.com>
2024-10-09 21:37:07 -0300

    Add new classes and something for front

---
+++ MobilemedCrud.Model/Entity/BookMarkModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilemedCrud.Model.Entity
+{
+    public class BookMarkModel
+    {
+        public int Id { get; set; }
+
+        public MedicModel Medic { get; set; }
+
+        public PatientModel Patient { get; set; }
+
+    }
+}
